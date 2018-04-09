2c173ef0710590c1851ac9e08b3bf75076acb549
omgitsmotrix <mohammedxec@gmail.com>
2018-04-09 21:27:34 +0100

    fixed ImageRepresentation unit-test

--- Tests/ImageRepresentationTest.py
+++ Tests/ImageRepresentationTest.py
@@ -2,7 +2,7 @@ import os
 import sys
 import unittest
 from ImageRepresentation import create_image
-sys.path.append(os.getcwd() + '\..\..\Libraries/chess_helper')
+sys.path.append(os.getcwd() + '..\..\Libraries\chess_helper')
 import chess
 
 
@@ -13,9 +13,9 @@ class ImageRepresentationTest(unittest.TestCase):
 		chess.load('rnbqkbnr/ppppppp1/7p/8/8/1P6/P1PPPPPP/RNBQKBNR w KQkq - 0 1')
 		self.test_setup = chess.get_setup()
 
-	@classmethod
-	def tearDownClass(cls):
-		os.remove(os.getcwd() + "\Resources\modifiedChessboard.png")
+	# @classmethod
+	# def tearDownClass(cls):
+	# 	os.remove(os.getcwd() + "\Resources\modifiedChessboard.png")
 
 	def test_create_image(self):
 		create_image(self.test_setup)
