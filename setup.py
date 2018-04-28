40ac239e9d58d0cef1eedfe39347f3925066c43e
omgitsmotrix <mohammedxec@gmail.com>
2018-04-28 15:00:13 +0100

    removed statistics test code

--- setup.py
+++ setup.py
@@ -4,19 +4,7 @@
 # packages = ["numpy"]
 # setup(name='test', version='1.0',options= {'build_exe': {'include_files': includeFiles, "packages":packages}} ,executables = [Executable('MainWindow.py')])
 
-"""
-hi
-saif
-"""
-
 from cx_Freeze import setup, Executable
 import os
 
 setup(name='test', version='1.0',executables = [Executable('MVC.py')])
-
-def test():
-	"""
-	yes
-	:return:
-	"""
-	print('hello')
